19b3a638e2a86c6d4d13fdbc3291f6cdad5c6a6f
Yixiong Jiang` <i@jiangyixiong.top>
2019-09-22 21:52:15 +0100

    first commit

---
+++ src/MinimumMovesToEqualArrayElements.java
@@ -0,0 +1,46 @@
+/*
+ * @Author: Gary J
+ * @Date: 2019-09-22 21:26:07
+ * @LastEditors: Gary J
+ * @LastEditTime: 2019-09-22 21:49:34
+ * @Description: file content
+ */
+import java.util.Arrays;
+
+public class MinimumMovesToEqualArrayElements {
+    // TIMEOUT
+    // public int minMoves(int[] nums){
+    //     int count = 0;
+    //     while(true){
+    //         Arrays.sort(nums);
+    //         boolean allEqual = true;
+    //         for (int num : nums) {
+    //             if(num!=nums[0]) allEqual = false;
+    //         }
+    //         if(allEqual) return count;
+    //         for(int i=0; i<nums.length-1;i++){
+    //             nums[i]++;
+    //         }
+    //         count++;
+    //     }
+        
+    // }
+        //将所有数值减小到最小值
+        //n-1个数+1 与 1个数-1 等效
+        // 只需计算需要将各个数各-1多少次
+        public int minMoves(int[] nums) {
+            int min = Integer.MAX_VALUE, count = 0;
+            for(int n : nums) {
+                min = Math.min(min, n);
+            }
+            for(int n : nums) {
+                count += n - min;
+            }
+            return count;
+        }
+    public static void main(String[] args) {
+        MinimumMovesToEqualArrayElements mmteae = new MinimumMovesToEqualArrayElements();
+        int[] nums = {1,2147483647};
+        System.out.println(mmteae.minMoves(nums));
+    }
+}
---
+++ src/RelativeRanks.java
@@ -0,0 +1,38 @@
+/*
+ * @Author: Gary J
+ * @Date: 2019-09-21 15:33:58
+ * @LastEditors: Gary J
+ * @LastEditTime: 2019-09-22 21:38:56
+ * @Description: file content
+ */
+import java.util.Arrays;
+class RelativeRanks{
+    public String[] findRelativeRanks(int[] nums){
+        int[] numsOrdered = nums.clone();
+        Arrays.sort(numsOrdered);
+        int length = nums.length;
+        String[] ranking = new String[length];
+        for(int i = 0;i<length;i++){
+            int rank = Arrays.binarySearch(numsOrdered, nums[i]);
+            if(rank == length-1){
+                ranking[i] = "Gold Medal";
+            }
+            else if(rank == length-2){
+                ranking[i] = "Silver Medal";
+            }
+            else if(rank == length-3){
+                ranking[i] = "Bronze Medal";
+            }
+            else{
+                ranking[i] = String.valueOf(length - rank);
+            }
+        }
+        return ranking;
+    }
+    // public static void main(String[] args) {
+    //     RelativeRanks rr = new RelativeRanks();
+    //     int[] nums = {1,2,3,4,5};
+    //     String[] returns = rr.findRelativeRanks(nums);
+    //     System.out.println("done");
+    // }
+}
