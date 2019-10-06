6b686aeecc399de6151325fbaeb0f05ef09b0f67
Yixiong Jiang` <i@jiangyixiong.top>
2019-10-06 20:42:02 +0100

    Add List Node

---
+++ src/main/ListNode.java
@@ -0,0 +1,12 @@
+/*
+ * @Author: Yixiong J
+ * @Date: 2019-10-06 20:40:19
+ * @LastEditors: Yixiong J
+ * @LastEditTime: 2019-10-06 20:41:46
+ * @Description: ListNode
+ */
+public class ListNode {
+    public int val;
+    public ListNode next;
+    public ListNode(int x){val = x;}
+}
